453b2872e165579f6e42fd159d66447cdfe40f87
Jia Wei <jiawei392@gmail.com>
2023-08-07 01:28:32 +0800

    Add search herbs function

---
+++ app/src/main/java/com/example/pinellia/ui/searchHerb/SearchHerbActivity.java
@@ -0,0 +1,115 @@
+package com.example.pinellia.ui.searchHerb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.example.pinellia.HerbDetails;
+import com.example.pinellia.adapter.HerbAdapter;
+import com.example.pinellia.databinding.ActivitySearchHerbBinding;
+import com.example.pinellia.model.Herb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchHerbActivity extends AppCompatActivity {
+
+    private ActivitySearchHerbBinding binding;
+    private SearchHerbViewModel searchHerbViewModel;
+    private HerbAdapter searchResultsAdapter;
+    private List<Herb> searchResults = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySearchHerbBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+        setContentView(root);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            setupSearchView(); // Call method to set up the search view in action bar
+        }
+
+        // Initialize the ViewModel
+        searchHerbViewModel = new ViewModelProvider(this).get(SearchHerbViewModel.class);
+
+        binding.recyclerViewSearchHerbs.setLayoutManager(new LinearLayoutManager(this));
+
+        // Reuse the HerbAdapter for search results
+        searchResultsAdapter = new HerbAdapter(searchResults);
+        binding.recyclerViewSearchHerbs.setAdapter(searchResultsAdapter);
+
+        // Set item click listener for search results
+        searchResultsAdapter.setOnItemClickListener(new HerbAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Herb herb) {
+                // Handle item click in search results to launch HerbDetails activity
+                Intent intent = new Intent(SearchHerbActivity.this, HerbDetails.class);
+                intent.putExtra("herb", herb);
+                startActivity(intent);
+            }
+        });
+
+        // Observe changes in search results LiveData
+        searchHerbViewModel.getSearchResultsLiveData().observe(this, new Observer<List<Herb>>() {
+            @Override
+            public void onChanged(List<Herb> herbs) {
+                // Update the UI with the new search results
+                searchResults.clear();
+                searchResults.addAll(herbs);
+                searchResultsAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void setupSearchView() {
+        SearchView searchView = new SearchView(getSupportActionBar().getThemedContext());
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // Set a listener for query text changes
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Handle query submission
+                // Close the keyboard
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Update search results based on query text change
+                searchHerbViewModel.performSearch(newText);
+                return true;
+            }
+        });
+
+        // Expand the SearchView by default
+        searchView.setIconifiedByDefault(false);
+
+        // Request focus and open the keyboard
+        searchView.requestFocus();
+
+        getSupportActionBar().setCustomView(searchView);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        // Handle the back button click in the action bar
+        onBackPressed();
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/pinellia/ui/searchHerb/SearchHerbViewModel.java
@@ -0,0 +1,95 @@
+package com.example.pinellia.ui.searchHerb;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.pinellia.model.Herb;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SearchHerbViewModel extends ViewModel {
+
+    private MutableLiveData<List<Herb>> herbListLiveData;
+    private MutableLiveData<List<Herb>> searchResultsLiveData;
+    private MutableLiveData<String> errorMessageLiveData;
+
+    public SearchHerbViewModel() {
+        // Load the herb list from Firebase when the ViewModel is created
+        getHerbList();
+    }
+
+    public LiveData<List<Herb>> getHerbList() {
+        if (herbListLiveData == null) {
+            herbListLiveData = new MutableLiveData<>();
+            searchResultsLiveData = new MutableLiveData<>();
+            errorMessageLiveData = new MutableLiveData<>();
+            loadHerbsFromFirebase();
+        }
+
+        return herbListLiveData;
+    }
+
+    private void loadHerbsFromFirebase() {
+        List<Herb> herbList = new ArrayList<>();
+
+        FirebaseDatabase.getInstance().getReference("herbs").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                herbList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Herb herb = snapshot.getValue(Herb.class);
+                    herbList.add(herb);
+                }
+
+                // Sort the herbList by name in ascending order (A-Z)
+                Collections.sort(herbList, new Comparator<Herb>() {
+                    @Override
+                    public int compare(Herb herb1, Herb herb2) {
+                        return herb1.getName().compareToIgnoreCase(herb2.getName());
+                    }
+                });
+
+                herbListLiveData.setValue(herbList);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                errorMessageLiveData.setValue(databaseError.getMessage());
+            }
+        });
+    }
+
+     void performSearch(String query) {
+
+        if (herbListLiveData == null || herbListLiveData.getValue() == null) {
+            return; // No herbs available
+        }
+
+        List<Herb> herbList = herbListLiveData.getValue();
+        List<Herb> searchResults = new ArrayList<>();
+
+        for (Herb herb : herbList) {
+            if (herb.getName().toLowerCase().contains(query.toLowerCase())) {
+                searchResults.add(herb);
+            }
+        }
+
+        searchResultsLiveData.setValue(searchResults);
+    }
+
+    public LiveData<List<Herb>> getSearchResultsLiveData() {
+        return searchResultsLiveData;
+    }
+
+    public LiveData<String> getErrorMessage() {
+        return errorMessageLiveData;
+    }
+}
