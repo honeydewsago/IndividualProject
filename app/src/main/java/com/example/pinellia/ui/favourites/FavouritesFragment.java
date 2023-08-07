bb13c66d78c475c1e1b38d164eff04a449f5f93f
Jia Wei <jiawei392@gmail.com>
2023-08-08 02:06:09 +0800

    Create herb details design

--- app/src/main/java/com/example/pinellia/ui/favourites/FavouritesFragment.java
+++ app/src/main/java/com/example/pinellia/ui/favourites/FavouritesFragment.java
@@ -25,7 +25,7 @@ public class FavouritesFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         // Set up the search bar
         setHasOptionsMenu(true);
     }
