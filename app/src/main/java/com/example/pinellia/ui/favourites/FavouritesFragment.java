855edfbe82aba881bbb60f3bb2f66bfb90d5cbe4
Jia Wei <jiawei392@gmail.com>
2023-08-07 20:15:54 +0800

    Create menu for favourites fragment

--- app/src/main/java/com/example/pinellia/ui/favourites/FavouritesFragment.java
+++ app/src/main/java/com/example/pinellia/ui/favourites/FavouritesFragment.java
@@ -2,19 +2,33 @@ package com.example.pinellia.ui.favourites;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pinellia.R;
 import com.example.pinellia.databinding.FragmentFavouritesBinding;
 
 public class FavouritesFragment extends Fragment {
 
     private FragmentFavouritesBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Set up the search bar
+        setHasOptionsMenu(true);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +42,34 @@ public class FavouritesFragment extends Fragment {
         favouritesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_favourites_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_browsing_history) {
+            // Handle Browsing History click
+            Toast.makeText(requireContext(), "Browsing History clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_settings) {
+            // Handle Settings click
+            Toast.makeText(requireContext(), "Settings clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_about) {
+            // Handle About click
+            Toast.makeText(requireContext(), "About clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public void onDestroyView() {
