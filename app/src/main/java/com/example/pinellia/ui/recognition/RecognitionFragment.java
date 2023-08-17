dd2e2d81e86306587600af33cbb787e4048dcd9a
Jia Wei <jiawei392@gmail.com>
2023-08-17 23:41:12 +0800

    Add function to process image

--- app/src/main/java/com/example/pinellia/ui/recognition/RecognitionFragment.java
+++ app/src/main/java/com/example/pinellia/ui/recognition/RecognitionFragment.java
@@ -2,11 +2,16 @@ package com.example.pinellia.ui.recognition;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,10 +26,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pinellia.TFLiteModelExecutor;
 import com.example.pinellia.databinding.FragmentRecognitionBinding;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.concurrent.ExecutorService;
@@ -32,10 +43,14 @@ import java.util.concurrent.Executors;
 
 public class RecognitionFragment extends Fragment {
 
+    private static final String TAG = "ProcessCaptureImage";
     private static final int REQUEST_CAMERA_PERMISSION = 1;
     private FragmentRecognitionBinding binding;
     private RecognitionViewModel recognitionViewModel;
     private ExecutorService cameraExecutor;
+    private TFLiteModelExecutor mTFLiteModelExecutor;
+    private int inputHeight = 224;
+    private int inputWidth  = 224;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -68,7 +83,96 @@ public class RecognitionFragment extends Fragment {
                     REQUEST_CAMERA_PERMISSION);
         }
 
-        binding.captureButton.setOnClickListener(v -> takePhoto());
+        binding.captureButton.setOnClickListener(v -> {
+            takePhoto(); // Capture the image
+            // Call the processCapturedImage function with the captured image path
+            processCapturedImage(recognitionViewModel.getCapturedImagePath());
+        });
+
+        // Initialize TFLiteModelExecutor using the model file from assets
+        String modelFileName = "model.tflite"; // Replace with your actual model file name
+        String modelAssetPath = getAssetFilePath(modelFileName);
+        mTFLiteModelExecutor = new TFLiteModelExecutor(modelAssetPath);
+    }
+
+    private String getAssetFilePath(String assetFileName) {
+        AssetManager assetManager = requireContext().getAssets();
+        try {
+            return assetManager.openFd(assetFileName).getFileDescriptor().toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void processCapturedImage(String imagePath) {
+        try {
+            Bitmap capturedBitmap = BitmapFactory.decodeFile(imagePath);
+            if (capturedBitmap != null) {
+                // Resize the bitmap to the required input size of tflite model
+                Bitmap resizedBitmap = Bitmap.createScaledBitmap(
+                        capturedBitmap, inputWidth, inputHeight, true);
+
+                // Convert the resized bitmap to a float array
+                float[] inputArray = convertBitmapToFloatArray(resizedBitmap);
+
+                // Make predictions using the TFLite model
+                float[] predictions = mTFLiteModelExecutor.runInference(inputArray);
+
+                // Process the predictions (e.g., find the highest confidence class)
+                int predictedClass = findPredictedClass(predictions);
+
+                // Perform actions based on the predicted class (e.g., update UI)
+                updateUIWithPrediction(predictedClass);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error processing captured image: " + e.getMessage());
+        }
+    }
+
+    private float[] convertBitmapToFloatArray(Bitmap bitmap) {
+        // Convert the bitmap to a float array and normalize pixel values
+        // Normalize pixel values to [0, 1] and reshape into a 1D array
+        float[] floatArray = new float[inputWidth * inputHeight * 3];
+        int index = 0;
+
+        for (int y = 0; y < inputHeight; y++) {
+            for (int x = 0; x < inputWidth; x++) {
+                int pixel = bitmap.getPixel(x, y);
+
+                // Normalize pixel values to the range [0, 1]
+                float red = ((pixel >> 16) & 0xFF) / 255.0f;
+                float green = ((pixel >> 8) & 0xFF) / 255.0f;
+                float blue = (pixel & 0xFF) / 255.0f;
+
+                // Add normalized values to the float array
+                floatArray[index++] = red;
+                floatArray[index++] = green;
+                floatArray[index++] = blue;
+            }
+        }
+
+        return floatArray;
+    }
+
+    private int findPredictedClass(float[] predictions) {
+        // Find the index of the class with the highest prediction score
+        int predictedClass = 0;
+        float maxScore = predictions[0];
+        for (int i = 1; i < predictions.length; i++) {
+            if (predictions[i] > maxScore) {
+                predictedClass = i;
+                maxScore = predictions[i];
+            }
+        }
+        return predictedClass;
+    }
+
+    private void updateUIWithPrediction(int predictedClass) {
+        // Update the UI based on the predicted class
+        // For example, update a TextView with the predicted herb's name
+        // ...
+        Toast.makeText(getContext(), "class: "+predictedClass, Toast.LENGTH_SHORT).show();
     }
 
     private void startCamera() {
