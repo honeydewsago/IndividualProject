4129ed7f070cc2a637d985471b0942b58a1171dd
Jia Wei <jiawei392@gmail.com>
2023-09-07 23:40:43 +0800

    Add user acknowledgement alert dialog in main activity

--- app/src/main/java/com/example/pinellia/MainActivity.java
+++ app/src/main/java/com/example/pinellia/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.pinellia;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +20,7 @@ import com.example.pinellia.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PREFS_NAME = "AppPreferences";
     private ActivityMainBinding binding;
 
     @Override
@@ -33,6 +39,39 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Check if the dialog should be shown
+        if (shouldShowDialog()) {
+            // Create and show the AlertDialog
+            AlertDialog alertDialog = new AlertDialog.Builder(this)
+                    .setTitle("User Acknowledgement")
+                    .setMessage("This app is designed for informational purposes and as a supplement to your wellness journey. While it provides insights into Traditional Chinese Medicine (TCM) and herbal remedies, it is not a substitute for professional medical advice, diagnosis, or treatment. Always consult with a qualified healthcare provider for any health-related concerns.")
+                    .setPositiveButton("I understand", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Handle the "OK" button click
+                            dialog.dismiss(); // Dismiss the dialog
+                            markDialogAsShown(); // Mark the dialog as shown
+                        }
+                    })
+                    .setCancelable(false) // Make the dialog non-cancelable
+                    .create();
+
+            alertDialog.show(); // Show the AlertDialog
+        }
     }
 
+    private boolean shouldShowDialog() {
+        // Use SharedPreferences to check if the dialog should be shown
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return !sharedPreferences.getBoolean("dialog_shown", false);
+    }
+
+    private void markDialogAsShown() {
+        // Use SharedPreferences to mark the dialog as shown
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("dialog_shown", true);
+        editor.apply();
+    }
 }
