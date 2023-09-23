fb40c4ec98fd7128ccb9c99f75913c709caa47c2
Jia Wei <jiawei392@gmail.com>
2023-09-24 02:43:48 +0800

    Create function to preprocess image to BGR

--- app/src/main/java/com/example/pinellia/ui/recognition/RecognitionResultsActivity.java
+++ app/src/main/java/com/example/pinellia/ui/recognition/RecognitionResultsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -19,6 +20,9 @@ import com.example.pinellia.R;
 import com.example.pinellia.databinding.ActivityRecognitionResultsBinding;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -69,7 +73,10 @@ public class RecognitionResultsActivity extends AppCompatActivity {
                     public void run() {
                         //UI Thread processing
                         if (tfliteModelExecutor != null) {
-                            classifyImage(imagePath);
+                            Bitmap bitmap = processImageToBGR(imagePath);
+                            binding.imageViewProcess.setImageBitmap(bitmap);
+
+                            classifyImage(bitmap);
 
                             // Hide the progress bar after classification is done
                             binding.progressBar.setVisibility(View.GONE);
@@ -80,23 +87,141 @@ public class RecognitionResultsActivity extends AppCompatActivity {
         });
     }
 
-    private void classifyImage(String path) {
+//    private void displayProcessedImage(String path) {
+//        Log.d("tflite", "Displaying processed image");
+//
+//        // Convert the captured image to a bitmap
+//        Bitmap capturedBitmap = BitmapFactory.decodeFile(path);
+//
+//        ByteBuffer processedBuffer = tfliteModelExecutor.preprocessImageForResNet50(capturedBitmap);
+//
+//        // Convert the ByteBuffer to float values
+//        float[] floatValues = new float[TFLiteModelExecutor.IMG_SIZE_X * TFLiteModelExecutor.IMG_SIZE_Y * TFLiteModelExecutor.PIXEL_SIZE];
+//        processedBuffer.rewind();
+//        processedBuffer.asFloatBuffer().get(floatValues);
+//
+//        // Convert float values to ARGB pixel values
+//        int[] intValues = new int[TFLiteModelExecutor.IMG_SIZE_X * TFLiteModelExecutor.IMG_SIZE_Y];
+//        for (int i = 0; i < intValues.length; ++i) {
+//            intValues[i] = Color.argb(255,
+//                    (int) (floatValues[i] * TFLiteModelExecutor.IMG_STD + TFLiteModelExecutor.IMG_MEAN),
+//                    (int) (floatValues[i + intValues.length] * TFLiteModelExecutor.IMG_STD + TFLiteModelExecutor.IMG_MEAN),
+//                    (int) (floatValues[i + 2 * intValues.length] * TFLiteModelExecutor.IMG_STD + TFLiteModelExecutor.IMG_MEAN));
+//        }
+//
+//        // Create a Bitmap from the ARGB pixel values
+//        Bitmap bitmap = Bitmap.createBitmap(intValues, TFLiteModelExecutor.IMG_SIZE_X, TFLiteModelExecutor.IMG_SIZE_Y, Bitmap.Config.ARGB_8888);
+//
+//        // Display the processed image in the ImageView
+//        binding.imageViewProcess.setImageBitmap(bitmap);
+//
+//    }
+
+    // Function to convert a ByteBuffer to a Bitmap
+//    private Bitmap convertByteBufferToBitmap(ByteBuffer buffer) {
+//        Log.d("tflite", "Converting byte buffer to bitmap");
+//        int[] intValues = new int[TFLiteModelExecutor.IMG_SIZE_X * TFLiteModelExecutor.IMG_SIZE_Y];
+//        float[] floatValues = new float[intValues.length];
+//
+//        // Convert the ByteBuffer to float values
+//        buffer.rewind();
+//        buffer.asFloatBuffer().get(floatValues);
+//
+//        // Convert float values to ARGB pixel values
+//        for (int i = 0; i < intValues.length; ++i) {
+//            intValues[i] = Color.argb(255,
+//                    (int) (floatValues[i] * TFLiteModelExecutor.IMG_STD + TFLiteModelExecutor.IMG_MEAN),
+//                    (int) (floatValues[i + intValues.length] * TFLiteModelExecutor.IMG_STD + TFLiteModelExecutor.IMG_MEAN),
+//                    (int) (floatValues[i + 2 * intValues.length] * TFLiteModelExecutor.IMG_STD + TFLiteModelExecutor.IMG_MEAN));
+//        }
+//
+//        // Create a Bitmap from the ARGB pixel values
+//        Bitmap bitmap = Bitmap.createBitmap(intValues, TFLiteModelExecutor.IMG_SIZE_X, TFLiteModelExecutor.IMG_SIZE_Y, Bitmap.Config.ARGB_8888);
+//
+//        return bitmap;
+//    }
+
+    private Bitmap processImageToBGR(String path) {
+        Log.d("tflite", "Displaying processed image");
+
+        // Convert the captured image to a bitmap
+        Bitmap capturedBitmap = BitmapFactory.decodeFile(path);
+
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(capturedBitmap, TFLiteModelExecutor.IMG_SIZE_X, TFLiteModelExecutor.IMG_SIZE_Y, true);
+
+        // Convert ARGB to BGR and normalize pixel values
+        int width = resizedBitmap.getWidth();
+        int height = resizedBitmap.getHeight();
+        int[] pixels = new int[width * height];
+        resizedBitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+
+        // Convert ARGB to BGR, subtract mean values, and normalize pixel values
+        for (int i = 0; i < pixels.length; i++) {
+            int pixel = pixels[i];
+            int blue = (pixel >> 16) & 0xFF;
+            int green = (pixel >> 8) & 0xFF;
+            int red = pixel & 0xFF;
+
+            // Convert to BGR format, subtract mean values, and normalize
+            float blueNorm = (blue - 103.939f) / 255.0f;
+            float greenNorm = (green - 116.779f) / 255.0f;
+            float redNorm = (red - 123.68f) / 255.0f;
+
+            // Clamp values to the range [0, 1]
+            blueNorm = Math.max(0.0f, Math.min(1.0f, blueNorm));
+            greenNorm = Math.max(0.0f, Math.min(1.0f, greenNorm));
+            redNorm = Math.max(0.0f, Math.min(1.0f, redNorm));
+
+            // Convert back to ARGB format
+            int blueFinal = (int) (blueNorm * 255.0f);
+            int greenFinal = (int) (greenNorm * 255.0f);
+            int redFinal = (int) (redNorm * 255.0f);
+
+            pixels[i] = 0xFF000000 | (redFinal << 16) | (greenFinal << 8) | blueFinal;
+        }
+
+        resizedBitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
+        return resizedBitmap;
+        // Display the processed image in the ImageView
+//        binding.imageViewProcess.setImageBitmap(resizedBitmap);
+    }
+
+    private void classifyImage(Bitmap bitmap) {
         if (tfliteModelExecutor == null) {
             Toast.makeText(this, "Uninitialized tflite model or invalid context.", Toast.LENGTH_SHORT).show();
             return;
         }
 
         // Convert the captured image to a bitmap
-        Bitmap capturedBitmap = BitmapFactory.decodeFile(path);
+//        Bitmap capturedBitmap = BitmapFactory.decodeFile(path);
 
         // Resize and preprocess the image
-        Bitmap resizedBitmap = Bitmap.createScaledBitmap(capturedBitmap, TFLiteModelExecutor.IMG_SIZE_X, TFLiteModelExecutor.IMG_SIZE_Y, true);
+//        Bitmap resizedBitmap = Bitmap.createScaledBitmap(capturedBitmap, TFLiteModelExecutor.IMG_SIZE_X, TFLiteModelExecutor.IMG_SIZE_Y, true);
 
-        String textToShow = tfliteModelExecutor.runInference(resizedBitmap);
-        resizedBitmap.recycle();
+        String textToShow = tfliteModelExecutor.runInference(bitmap);
+        bitmap.recycle();
 
         binding.textViewResults.setText(textToShow);
     }
+
+//    private void classifyImage(String path) {
+//        if (tfliteModelExecutor == null) {
+//            Toast.makeText(this, "Uninitialized tflite model or invalid context.", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//
+//        // Convert the captured image to a bitmap
+//        Bitmap capturedBitmap = BitmapFactory.decodeFile(path);
+//
+//        // Resize and preprocess the image
+//        Bitmap resizedBitmap = Bitmap.createScaledBitmap(capturedBitmap, TFLiteModelExecutor.IMG_SIZE_X, TFLiteModelExecutor.IMG_SIZE_Y, true);
+//
+//        String textToShow = tfliteModelExecutor.runInference(resizedBitmap);
+//        resizedBitmap.recycle();
+//
+//        binding.textViewResults.setText(textToShow);
+//    }
 
     private void loadAndDisplayImage(String imagePath) {
         if (imagePath == null) {
--- app/src/main/java/com/example/pinellia/ui/recognition/TFLiteModelExecutor.java
+++ app/src/main/java/com/example/pinellia/ui/recognition/TFLiteModelExecutor.java
@@ -3,6 +3,7 @@ package com.example.pinellia.ui.recognition;
 import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.SystemClock;
 import android.util.Log;
 
@@ -25,7 +26,7 @@ import java.util.PriorityQueue;
 
 public class TFLiteModelExecutor {
 
-    private static final String MODEL_PATH = "model5.tflite";
+    private static final String MODEL_PATH = "model9.tflite";
     private static final String LABEL_PATH = "labels.txt";
 
     private Interpreter tflite;
@@ -33,11 +34,11 @@ public class TFLiteModelExecutor {
     private ByteBuffer imageData = null;
     private static final int RESULTS_TO_SHOW = 15;
     private static final int BATCH_SIZE = 1;
-    private static final int PIXEL_SIZE = 3;
+    static final int PIXEL_SIZE = 3;
     static final int IMG_SIZE_X = 224;
     static final int IMG_SIZE_Y = 224;
-    private static final int IMG_MEAN = 224;
-    private static final float IMG_STD = 224.0f;
+    static final int IMG_MEAN = 224;
+    static final float IMG_STD = 224.0f;
     private int[] intValues = new int[IMG_SIZE_X * IMG_SIZE_Y];
     private float[][] labelProbArray = null;
 
@@ -48,7 +49,7 @@ public class TFLiteModelExecutor {
         imageData.order(ByteOrder.nativeOrder());
         labelProbArray = new float[1][labelList.size()];
 
-        Log.d("tflite", "Created a TFLite Model.");
+        Log.d("tflite", "Created a TFLite Model with "+MODEL_PATH);
     }
 
     private MappedByteBuffer loadModelFile(Activity activity) throws IOException {
@@ -77,8 +78,12 @@ public class TFLiteModelExecutor {
             Log.e("tflite", "TFLite Model has not been initialized. Unable to run inference.");
             return "Uninitialized TFLite Model";
         }
-        convertBitmapToByteBuffer(bitmap);
-        // Here's where the magic happens!!!
+
+//        convertBitmapToByteBuffer(bitmap);
+
+//        processImageForResNet50(bitmap);
+        imageData = preprocessImageForResNet50(bitmap);
+
         long startTime = SystemClock.uptimeMillis();
         tflite.run(imageData, labelProbArray);
         long endTime = SystemClock.uptimeMillis();
@@ -107,6 +112,59 @@ public class TFLiteModelExecutor {
             }
         }
     }
+
+    public ByteBuffer preprocessImageForResNet50(Bitmap bitmap) {
+        // Resize the image to the expected input size (IMG_SIZE_X x IMG_SIZE_Y)
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, IMG_SIZE_X, IMG_SIZE_Y, true);
+
+        // Create a ByteBuffer to hold the processed image data
+        ByteBuffer inputBuffer = ByteBuffer.allocateDirect(4 * BATCH_SIZE * IMG_SIZE_X * IMG_SIZE_Y * PIXEL_SIZE);
+        inputBuffer.order(ByteOrder.nativeOrder());
+        inputBuffer.rewind();
+
+        // Normalize pixel values and convert RGB to BGR
+        int pixel = 0;
+        for (int i = 0; i < IMG_SIZE_X; ++i) {
+            for (int j = 0; j < IMG_SIZE_Y; ++j) {
+                int color = resizedBitmap.getPixel(i, j);
+
+                // Convert RGB to BGR
+                float blue = ((color >> 16) & 0xFF) - 123.68f;
+                float green = ((color >> 8) & 0xFF) - 116.779f;
+                float red = (color & 0xFF) - 103.939f;
+
+                // Store BGR values
+                inputBuffer.putFloat(blue / 255.0f);
+                inputBuffer.putFloat(green / 255.0f);
+                inputBuffer.putFloat(red / 255.0f);
+            }
+        }
+
+        return inputBuffer;
+    }
+
+
+//    public Bitmap processImageForResNet50(Bitmap bitmap) {
+//        // Create a Mat object from the Bitmap
+//        Mat mat = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC3);
+//
+//        // Convert the Bitmap to BGR format
+//        Utils.bitmapToMat(bitmap, mat);
+//        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGBA2BGR);
+//
+//        // Zero-center the image with respect to the ImageNet dataset
+//        Scalar mean = new Scalar(104, 117, 123); // These values are specific to ImageNet
+//        Core.subtract(mat, mean, mat);
+//
+//        // Ensure the Mat is in the correct data type (float32)
+//        mat.convertTo(mat, CvType.CV_32FC3);
+//
+//        // Convert the Mat back to a Bitmap
+//        Bitmap processedBitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888);
+//        Utils.matToBitmap(mat, processedBitmap);
+//
+//        return processedBitmap;
+//    }
 
     private String printTopKLabels() {
         for (int i = 0; i < labelList.size(); ++i) {
