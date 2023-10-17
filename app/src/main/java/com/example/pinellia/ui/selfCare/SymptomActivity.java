8b139b28519b33426b317d2a0eddf58b8a9b7259
Jia Wei <jiawei392@gmail.com>
2023-10-18 01:38:43 +0800

    Add action bar and navigation in symptom and usage activity

--- app/src/main/java/com/example/pinellia/ui/selfCare/SymptomActivity.java
+++ app/src/main/java/com/example/pinellia/ui/selfCare/SymptomActivity.java
@@ -3,14 +3,33 @@ package com.example.pinellia.ui.selfCare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.pinellia.R;
+import com.example.pinellia.databinding.ActivityRecognitionResultsBinding;
+import com.example.pinellia.databinding.ActivitySymptomBinding;
 
 public class SymptomActivity extends AppCompatActivity {
 
+    private ActivitySymptomBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_symptom);
+
+        binding = ActivitySymptomBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Enable action bar title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Symptom");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        // Handle the back button click in the action bar
+        onBackPressed();
+        return true;
     }
 }
--- app/src/main/java/com/example/pinellia/ui/selfCare/UsageActivity.java
+++ app/src/main/java/com/example/pinellia/ui/selfCare/UsageActivity.java
@@ -5,12 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.pinellia.R;
+import com.example.pinellia.databinding.ActivitySymptomBinding;
+import com.example.pinellia.databinding.ActivityUsageBinding;
 
 public class UsageActivity extends AppCompatActivity {
 
+    private ActivityUsageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_usage);
+
+        binding = ActivityUsageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Enable action bar title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Usage");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        // Handle the back button click in the action bar
+        onBackPressed();
+        return true;
     }
 }
