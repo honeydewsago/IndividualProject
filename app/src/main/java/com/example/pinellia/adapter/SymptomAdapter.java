86e023b969278256538a2807e2ffe58a3dc15759
Jia Wei <jiawei392@gmail.com>
2023-10-18 15:54:09 +0800

    Create recycleview to display list of symptoms button

---
+++ app/src/main/java/com/example/pinellia/adapter/SymptomAdapter.java
@@ -0,0 +1,58 @@
+package com.example.pinellia.adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pinellia.R;
+import com.example.pinellia.databinding.ItemSymptomButtonBinding;
+import java.util.List;
+
+public class SymptomAdapter extends RecyclerView.Adapter<SymptomAdapter.SymptomViewHolder> {
+
+    private List<String> symptomsList;
+
+    public SymptomAdapter(List<String> symptomsList) {
+        this.symptomsList = symptomsList;
+    }
+
+    @Override
+    public SymptomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemSymptomButtonBinding binding = ItemSymptomButtonBinding.inflate(layoutInflater, parent, false);
+        return new SymptomAdapter.SymptomViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SymptomViewHolder holder, int position) {
+        String symptom = symptomsList.get(position);
+        holder.binding.toggleButtonSymptom.setText(symptom);
+        holder.binding.toggleButtonSymptom.setTextOn(symptom);
+        holder.binding.toggleButtonSymptom.setTextOff(symptom);
+
+        // Set an OnCheckedChangeListener to handle the color change
+        holder.binding.toggleButtonSymptom.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                holder.binding.toggleButtonSymptom.setBackgroundResource(R.drawable.symptom_toogle_button);
+            } else {
+                holder.binding.toggleButtonSymptom.setBackgroundResource(R.drawable.symptom_toogle_button);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return symptomsList.size();
+    }
+
+    public class SymptomViewHolder extends RecyclerView.ViewHolder {
+        private final ItemSymptomButtonBinding binding;
+
+        public SymptomViewHolder(ItemSymptomButtonBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
--- app/src/main/java/com/example/pinellia/ui/selfCare/SymptomActivity.java
+++ app/src/main/java/com/example/pinellia/ui/selfCare/SymptomActivity.java
@@ -1,17 +1,26 @@
 package com.example.pinellia.ui.selfCare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.pinellia.R;
+import com.example.pinellia.adapter.HerbAdapter;
+import com.example.pinellia.adapter.SymptomAdapter;
 import com.example.pinellia.databinding.ActivityRecognitionResultsBinding;
 import com.example.pinellia.databinding.ActivitySymptomBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SymptomActivity extends AppCompatActivity {
 
     private ActivitySymptomBinding binding;
+    private SymptomAdapter symptomAdapter;
+    private List<String> symptomsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,19 @@ public class SymptomActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("Symptom");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        String[] symptomsArray = {
+                "Abdominal Pain", "Bleeding", "High Blood Pressure", "Cold", "Cough", "Constipation", "Cramps",
+                "Dampness", "Diarrhea", "Menstruation Pain/Cramps", "Nose Bleed", "Fatigue", "Fever", "Gastric",
+                "Insomnia", "Itching", "Phlegm", "Swelling", "Yin Deficiency"
+        };
+
+        symptomsList = new ArrayList<>(Arrays.asList(symptomsArray));
+
+        // Set up RecyclerView to display favorite herbs
+        binding.recyclerViewSymptom.setLayoutManager(new LinearLayoutManager(this));
+        symptomAdapter = new SymptomAdapter(symptomsList);
+        binding.recyclerViewSymptom.setAdapter(symptomAdapter);
     }
 
     @Override
